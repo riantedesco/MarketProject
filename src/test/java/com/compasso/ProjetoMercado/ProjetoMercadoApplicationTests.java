b9d28cf023cfb81ba2754fce13db945f9429deb8
Rian Tedesco <rct.tedesco@gmail.com>
2021-11-16 14:15:21 -0300

    Projeto base

---
+++ src/test/java/com/compasso/ProjetoMercado/ProjetoMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compasso.ProjetoMercado;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
